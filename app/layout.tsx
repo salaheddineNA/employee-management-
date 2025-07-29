050856215885ec403e934fee636798f35e460079
salaheddineNA <160613484+salaheddineNA@users.noreply.github.com>
2025-07-29 19:23:55 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Gestion des Employés',
+  description: 'Gestion des Employés',
+  generator: 'Gestion des Employés',
 }
 
 export default function RootLayout({
